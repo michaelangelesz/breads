976d2cea5d3d3a92928b4ad218b8805103c04e2c
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-07 14:08:03 -0400

    Add Bread READ Route: Index

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 // DEPENDENCIES
-const express = require('express')
+const express = require("express");
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads");
+});
+
+// Breads
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-})
+  console.log("listening on port", PORT);
+});
